98190df8adcc29d533ff996d93059fbb4e60a18a
tjb <35938639+whothere788@users.noreply.github.com>
2018-11-01 14:35:31 -0400

    dadscomp upload

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    static public bool goalMet = false;
+    void OnTriggerEnter(Collider other)
+    {
+     if (other.gameObject.tag == "Projectile")
+        {
+            Goal.goalMet = true;
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        } 
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
